f12d30662d51da952851454c4c1788b4f49ca9fe
sekomidev <68436279+sekomidev@users.noreply.github.com>
2022-09-01 11:54:02 +0300

    Created an example project

--- examples/UserDataSavingExample/Program.cs
+++ examples/UserDataSavingExample/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace UserDataSavingExample
+﻿using DataManagement;
+namespace UserDataSavingExample
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			
+			User user0 = new(name: "John Doe", password: "1234");
+			// saves the user0 data to "user.xml" file
+			DataManager.Save(@".\user.xml", user0);
+
+			User user1 = DataManager.Load<User>(@".\user.xml");
+			// will output "John Doe"
+			Console.WriteLine(user1.Name);
 		}
 	}
 }
---
+++ examples/UserDataSavingExample/User.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml.Serialization;
+
+namespace UserDataSavingExample
+{
+	/// <summary>
+	/// This is an example class for save/load method demonstration purposes.
+	/// </summary>
+	public class User
+	{
+		// this field will not be saved
+		private const int someFieldThatWillNotBeSaved = 10;
+
+		// this property will be saved
+		public string Name { get; set; }
+		// this property will also be saved
+		// (don't store real passwords in plain text, please)
+		public string Password { get; set; }
+		// this property will not be saved
+		[XmlIgnore]
+		public string SessionId { get; private set; }
+		
+		// a parameterless constructor is needed to save/load objects
+		public User()
+		{
+			Name = String.Empty;
+			Password = String.Empty;
+			SessionId = Guid.NewGuid().ToString();
+		}
+		public User(string name, string password)
+		{
+			Name = name;
+			Password = password;
+			SessionId = Guid.NewGuid().ToString();
+		}
+	}
+}
