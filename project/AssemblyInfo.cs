85a46dfe2f2fc47d70dbc9e335d00ae4d908570d
sekomidev <68436279+sekomidev@users.noreply.github.com>
2023-03-24 18:39:53 +0200

    Delete AssemblyInfo.cs

--- project/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.InteropServices;
-[assembly: ComVisible(false)]
-[assembly: Guid("a84178d1-b910-4b25-9ea9-a9aba45a1819")]
-[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
-
